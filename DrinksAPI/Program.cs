81a95e3e5093cde7df2e74d5edeefb73e0dfd78e
Ismail M <im_077@hotmail.com>
2022-08-07 13:22:00 +0200

    Add project files.

---
+++ DrinksAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+//var client = new RestClient("https://jsonplaceholder.typicode.com/posts");
+//var payload = new JObject();
+//payload.Add("title", "some cool post title");
+//payload.Add("body", "some cool body content");
+//var requset = new RestRequest();
+//requset.AddStringBody(payload.ToString(), DataFormat.Json);
+//var result = client.PostAsync(requset).Result;
+
+
+//getEmployeeData();
+
+//static void getEmployeeData()
+//{
+//    var client = new RestClient("https://dummy.restapiexample.com/api/v1");
+//    var request = new RestRequest("employees");
+//    var response = client.Execute(request);
+
+//    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+//    {
+//        string rawResponse = response.Content;
+//        var result = JsonConvert.DeserializeObject<Rootobject>(rawResponse);
+//    }
+
+
+
---
+++ DrinksAPI/TableVisualition.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleTableExt;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksAPI
+{
+    public class TableVisualition
+    {
+        //public static void ShowTable(List<> tableData)
+        //{
+        //    Console.WriteLine("\n\n");
+
+        //    ConsoleTableBuilder
+        //        .From(tableData)
+        //        .WithTitle("Employees")
+        //        .ExportAndWriteLine();
+        //    Console.WriteLine("\n\n");
+        //}
+    }
+}
